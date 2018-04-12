bf025c315748cef3e4a00ada9eedcdc416453159
Decoder22 <brianjohncox232@gmail.com>
2018-04-12 18:01:18 -0400

    include firebase
    npm installed firebase so it connects now

--- MusikTalk/src/app/app.module.ts
+++ MusikTalk/src/app/app.module.ts
@@ -1,8 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 import { AppComponent } from './app.component';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyA_nU_3UBcDX7PV5oLzxoJuyggNoPPtjHA",
+  authDomain: "musiktalk-6b659.firebaseapp.com",
+  databaseURL: "https://musiktalk-6b659.firebaseio.com",
+  projectId: "musiktalk-6b659",
+  storageBucket: "musiktalk-6b659.appspot.com",
+  messagingSenderId: "140711104501"
+};
 
 
 @NgModule({
@@ -10,7 +20,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),  // Add this
+    AngularFirestoreModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
